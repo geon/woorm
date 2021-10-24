fda35dadb9f15b00dbe83b7847c4156eb04b412a
Victor Widell <victor.widell@gmail.com>
2021-10-24 18:58:36 +0200

    Added test Pop element from circularBuffer.

--- circular-buffer.c
+++ circular-buffer.c
@@ -6,4 +6,10 @@ void circularBufferInit(CircularBuffer *circularBuffer)
 
 void circularBufferPush(CircularBuffer *circularBuffer, uint8_t value)
 {
+	circularBuffer->value = value;
+}
+
+uint8_t circularBufferPop(CircularBuffer *circularBuffer)
+{
+	return circularBuffer->value;
 }
--- circular-buffer.h
+++ circular-buffer.h
@@ -5,10 +5,11 @@
 
 typedef struct CircularBuffer
 {
-    uint8_t placeholder;
+    uint8_t value;
 } CircularBuffer;
 
 void circularBufferInit(CircularBuffer *circularBuffer);
 void circularBufferPush(CircularBuffer *circularBuffer, uint8_t value);
+uint8_t circularBufferPop(CircularBuffer *circularBuffer);
 
 #endif
