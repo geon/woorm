b8691f13759ed8a54a87ecd9d404a185d902d937
Victor Widell <victor.widell@gmail.com>
2021-10-27 10:06:02 +0200

    Added Direction enum.

---
+++ direction.h
@@ -0,0 +1,14 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+typedef enum Direction
+{
+	Direction_first,
+	Direction_up = 0,
+	Direction_right,
+	Direction_down,
+	Direction_left,
+	Direction_count
+} Direction;
+
+#endif
