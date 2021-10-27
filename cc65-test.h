5035f3f7e0d9c29e938993c1819c5e48a47cbb6a
Victor Widell <victor@topmost.se>
2021-10-27 23:51:07 +0200

    Added test: Convert coord and position.

--- cc65-test.h
+++ cc65-test.h
@@ -5,8 +5,11 @@ void beforeTests();
 void afterTests();
 void beginTest(char *messageString);
 void endTest();
+
 void assertWord(char *messageString, uint16_t result, uint16_t okValue);
 void assertByte(char *messageString, uint8_t result, uint8_t okValue);
 void assertWordDecimal(char *messageString, uint16_t result, uint16_t okValue);
 void assertByteDecimal(char *messageString, uint8_t result, uint8_t okValue);
 void assertTrue(char *messageString, bool success);
+
+void fail(char *messageString, char *result, char *okValue);
