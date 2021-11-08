15d6915afdf87913292a41c622d8acb692708d03
Victor Widell <victor@topmost.se>
2021-11-08 18:34:19 +0100

    Added test: A worm is drawn in 4 tiles on init.

--- worm.c
+++ worm.c
@@ -1,5 +1,25 @@
 #include "worm.h"
+#include "coord.h"
+#include "direction.h"
+#include "screen.h"
+#include "tile.h"
+#include <stdint.h>
 
-void wormInit(Worm *worm)
+void wormDraw(Worm *worm, Screen *screen, uint16_t pos, Direction direction);
+
+void wormInit(Worm *worm, Screen *screen, uint16_t pos, Direction direction)
 {
+	wormDraw(worm, screen, pos, direction);
+}
+
+void wormDraw(Worm *worm, Screen *screen, uint16_t pos, Direction direction)
+{
+	int part;
+
+	for (part = TileType_animated_head; part <= TileType_animated_end; ++part)
+	{
+		screen->chars[pos] = tileCreate(part, direction, direction, Microstep_0);
+		screen->colors[pos] = 14;
+		pos -= getPositionOffsetForDirection(direction);
+	}
 }
