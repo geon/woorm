362104a4e165768da8c05212ea89e563ae74fe3f
Victor Widell <victor.widell@gmail.com>
2021-12-25 17:31:20 +0100

    Refactor. Moved all level initialization to own method.

--- level.c
+++ level.c
@@ -1,5 +1,4 @@
 #include "level.h"
-#include "screen.h"
 #include <c64.h>
 
 void levelDraw(Level *level, Screen *screen)
@@ -11,4 +10,13 @@ void levelDraw(Level *level, Screen *screen)
 		screen->chars[i] = level->chars[i];
 		screen->colors[i] = level->colors[i];
 	}
+}
+
+void levelStart(Level *level, Screen *screen, Worm worms[4])
+{
+	levelDraw(level, screen);
+	wormInit(&worms[0], screen, coordToPos(level->playerStarts[0].position), level->playerStarts[0].direction, COLOR_CYAN + 8);
+	wormInit(&worms[1], screen, coordToPos(level->playerStarts[1].position), level->playerStarts[1].direction, COLOR_GREEN + 8);
+	wormInit(&worms[2], screen, coordToPos(level->playerStarts[2].position), level->playerStarts[2].direction, COLOR_YELLOW + 8);
+	wormInit(&worms[3], screen, coordToPos(level->playerStarts[3].position), level->playerStarts[3].direction, COLOR_RED + 8);
 }
