f0ec97a2491e0e5cd5f88f8727581c3c0318b656
Victor Widell <victor.widell@gmail.com>
2021-12-25 16:55:36 +0100

    Added Level struct with player start positions.

---
+++ level.h
@@ -0,0 +1,23 @@
+#ifndef LEVEL_H
+#define LEVEL_H
+
+#include "coord.h"
+#include "direction.h"
+#include <stdint.h>
+
+typedef struct PlayerStart
+{
+	Coord position;
+	Direction direction;
+} PlayerStart;
+
+typedef uint8_t LevelAttributes[1000];
+
+typedef struct Level
+{
+	PlayerStart playerStarts[4];
+	LevelAttributes chars;
+	LevelAttributes colors;
+} Level;
+
+#endif
