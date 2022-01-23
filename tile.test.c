07619b312356465946a86dab84fc24c37431f605
Victor Widell <victor.widell@gmail.com>
2022-01-23 17:53:44 +0100

    Added tests: Create tile up step 1-3.

--- tile.test.c
+++ tile.test.c
@@ -12,4 +12,31 @@ void tileTest()
 		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_up, Direction_up, Microstep_0), Tile_empty);
 	}
 	endTest();
+
+	beginTest("Create tile up step 1.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_up, Direction_up, Microstep_1), Tile_worm_up_0_1);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_up, Direction_up, Microstep_1), Tile_worm_up_1_1);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_up, Microstep_1), Tile_worm_up_2_1);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_up, Direction_up, Microstep_1), Tile_worm_up_3_1);
+	}
+	endTest();
+
+	beginTest("Create tile up step 2.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_up, Direction_up, Microstep_2), Tile_worm_up_0_2);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_up, Direction_up, Microstep_2), Tile_worm_up_1_2);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_up, Microstep_2), Tile_worm_up_2_2);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_up, Direction_up, Microstep_2), Tile_worm_up_3_2);
+	}
+	endTest();
+
+	beginTest("Create tile up step 3.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_up, Direction_up, Microstep_3), Tile_worm_up_0_3);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_up, Direction_up, Microstep_3), Tile_worm_up_1_3);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_up, Microstep_3), Tile_worm_up_2_3);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_up, Direction_up, Microstep_3), Tile_worm_up_3_3);
+	}
+	endTest();
 }
