73fba8ce96ea1c6824b4726cacea4b015cf887bb
Victor Widell <victor@topmost.se>
2022-01-30 12:40:59 +0100

    Added test: Create tile anti-c.w. step 0-3.

--- tile.test.c
+++ tile.test.c
@@ -183,4 +183,40 @@ void tileTest()
 		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_down, Direction_left, Microstep_3), Tile_worm_down_left_3_3);
 	}
 	endTest();
+
+	beginTest("Create tile anti-c.w. step 0.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_down, Direction_right, Microstep_0), Tile_worm_down_right_0_0);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_left, Direction_down, Microstep_0), Tile_worm_left_down_1_0);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_left, Microstep_0), Tile_worm_up_left_2_0);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_right, Direction_up, Microstep_0), Tile_empty);
+	}
+	endTest();
+
+	beginTest("Create tile anti-c.w. step 1.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_down, Direction_right, Microstep_1), Tile_worm_down_right_0_1);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_left, Direction_down, Microstep_1), Tile_worm_left_down_1_1);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_left, Microstep_1), Tile_worm_up_left_2_1);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_right, Direction_up, Microstep_1), Tile_worm_right_up_3_1);
+	}
+	endTest();
+
+	beginTest("Create tile anti-c.w. step 2.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_down, Direction_right, Microstep_2), Tile_worm_down_right_0_2);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_left, Direction_down, Microstep_2), Tile_worm_left_down_1_2);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_left, Microstep_2), Tile_worm_up_left_2_2);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_right, Direction_up, Microstep_2), Tile_worm_right_up_3_2);
+	}
+	endTest();
+
+	beginTest("Create tile anti-c.w. step 3.");
+	{
+		assertByteDecimal("Head", tileCreate(TileType_animated_head, Direction_down, Direction_right, Microstep_3), Tile_worm_down_right_0_3);
+		assertByteDecimal("Head to middle", tileCreate(TileType_animated_headToMiddle, Direction_left, Direction_down, Microstep_3), Tile_worm_left_down_1_3);
+		assertByteDecimal("End to middle", tileCreate(TileType_animated_endToMiddle, Direction_up, Direction_left, Microstep_3), Tile_worm_up_left_2_3);
+		assertByteDecimal("End", tileCreate(TileType_animated_end, Direction_right, Direction_up, Microstep_3), Tile_worm_right_up_3_3);
+	}
+	endTest();
 }
