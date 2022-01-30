ac9a42ab12ccf17a36edc9f04d591883b2c8f150
Victor Widell <victor.widell@gmail.com>
2022-01-30 20:17:36 +0100

    Added test: To move, it must add a tile in front.

--- worm.h
+++ worm.h
@@ -1,15 +1,28 @@
 #ifndef WORM_H
 #define WORM_H
 
+#include "circular-buffer.h"
 #include "direction.h"
+#include "microstep.h"
 #include "screen.h"
 #include <stdint.h>
 
+typedef struct TailCell
+{
+	Direction direction;
+	uint16_t position;
+} TailCell;
+
 typedef struct Worm
 {
-	char placeholder;
+	Screen *screen;
+	Direction nextDirection;
+	Microstep step;
+	CircularBuffer tail;
+	TailCell tailValues[0x100];
 } Worm;
 
 void wormInit(Worm *worm, Screen *screen, uint16_t pos, Direction direction);
+void wormStep(Worm *worm);
 
 #endif
