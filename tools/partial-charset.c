8f444c545247eed55dbf13844adc376e8ec56496
Victor Widell <victor.widell@gmail.com>
2022-04-02 15:46:40 +0200

    Added assertions.

--- tools/partial-charset.c
+++ tools/partial-charset.c
@@ -2,6 +2,14 @@
 
 int partialCharsetFindIndexOrAdd(PartialCharset *charset, CharsetChar charsetChar)
 {
+	// Avoid adding duplicates.
+	int index = charsetIndexOf(charset->charset, charsetChar);
+	if (index != -1 && index < charset->numUsedChars)
+	{
+		return index;
+	}
+
+	// New unique char, so add it.
 	charsetCharCopy(charsetChar, charset->charset[charset->numUsedChars]);
 	return charset->numUsedChars++;
 }
