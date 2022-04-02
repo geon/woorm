6eff05f236558153f20fc3c7b5afd5e4d530aa80
Victor Widell <victor.widell@gmail.com>
2022-04-02 13:15:57 +0200

    Added tool test: Charset indexOf.

--- cc65-test.h
+++ cc65-test.h
@@ -11,5 +11,6 @@ void assertByte(char *messageString, uint8_t result, uint8_t okValue);
 void assertWordDecimal(char *messageString, uint16_t result, uint16_t okValue);
 void assertByteDecimal(char *messageString, uint8_t result, uint8_t okValue);
 void assertTrue(char *messageString, bool success);
+void assertIntDecimal(char *messageString, int result, int okValue);
 
 void fail(char *messageString, char *result, char *okValue);
---
+++ tools/charset.test.h
@@ -0,0 +1,6 @@
+#ifndef CHARSET_TEST_H
+#define CHARSET_TEST_H
+
+void charsetTest();
+
+#endif
