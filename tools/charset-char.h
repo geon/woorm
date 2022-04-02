eb571e883b9322c2033222a370eb4597c0371933
Victor Widell <victor@topmost.se>
2022-04-02 12:51:22 +0200

    Added tool test: CharsetChar equals.

---
+++ tools/charset-char.h
@@ -0,0 +1,11 @@
+#ifndef CHARSET_CHAR_H
+#define CHARSET_CHAR_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef uint8_t CharsetChar[8];
+
+bool charsetCharEquals(CharsetChar a, CharsetChar b);
+
+#endif
---
+++ tools/charset-char.test.h
@@ -0,0 +1,6 @@
+#ifndef CHARSET_CHAR_TEST_H
+#define CHARSET_CHAR_TEST_H
+
+void charsetCharTest();
+
+#endif
