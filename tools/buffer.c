a5efa098f0f8847741b7f69e8e191ce0abdba453
Victor Widell <victor.widell@gmail.com>
2022-04-19 17:17:26 +0200

    Added test: Create buffer from string.

--- tools/buffer.c
+++ tools/buffer.c
@@ -1,9 +1,18 @@
 #include "buffer.h"
+#include <string.h>
 
 Buffer bufferCreate(uint8_t *content, size_t length)
 {
 	Buffer buffer;
 	buffer.content = content;
 	buffer.length = length;
+	return buffer;
+}
+
+Buffer bufferCreateFromString(char string[])
+{
+	Buffer buffer;
+	buffer.content = (uint8_t *)string;
+	buffer.length = strlen(string);
 	return buffer;
 }
