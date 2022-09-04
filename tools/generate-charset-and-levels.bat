1822f960ac9fd7135dde673df787f4646951e5e7
Victor Widell <victor.widell@gmail.com>
2022-09-04 09:33:12 +0200

    Refactor. Combined both tools into one.

---
+++ tools/generate-charset-and-levels.bat
@@ -0,0 +1,18 @@
+cl ^
+    buffer.c ^
+    ../lz77-back-reference.c ^
+    lz77-back-reference.tools.c ^
+    lz77.c ^
+    level.c ^
+    uncompressed-levels.c ^
+    original-worm-charset.c ^
+    generate-charset-and-levels.c ^
+    ../tile.c ^
+    charset-char.c ^
+    charset.c ^
+    partial-charset.c ^
+    tile.legacy.c ^
+    -o generate-charset-and-levels.exe
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+generate-charset-and-levels.exe
